f1c56769b9e6381d9033257f0f2b11943ca2fe79
Asen Iliev <assen.s.iliev@gmail.com>
2023-11-10 00:19:24 +0100

    end of 09-11-2023

--- Assets/_Dead Earth/Editor/AIWaypointsEditor.cs
+++ Assets/_Dead Earth/Editor/AIWaypointsEditor.cs
@@ -4,9 +4,40 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.AI;
 
-[CustomEditor(typeof (AIWaypointNetwork))]
+[CustomEditor(typeof (AIWaypointNetwork))] // This is the component on which the editor methods will act
+// For info: we want to have special rendering in the scene view and in the inspector for all the
+// objects that have 'AIWaypointNetwork' script attached to them
+// The OSceneGUI method will render lines with the path connecting the view points
+// The OnInspectorGUI method will render a special inspector's content (with a slider clamping 
+// selected view points.
+
 public class AIWaypointsEditor : Editor
 {
+    public override void OnInspectorGUI()
+    {
+        // The OnIspectorGUI method overrides the way that a component is represented in inspector
+        // It removes all the elements that are usually available for the inspector and coming from the 
+        // corresponding component. If this method is let empty, simply no info at all will be
+        // displayed in the inspector for the particular component (AIWaypointNetowrk in this case)
+
+        // Get reference to the AIWaypointNetwork class:
+        AIWaypointNetwork network = (AIWaypointNetwork)target;
+
+        // display the drop-down control with the different display modes
+        network.displayMode = (PathDisplayMode)EditorGUILayout.EnumPopup("Display mode", network.displayMode);
+
+        if(network.displayMode == PathDisplayMode.Paths)
+        {
+            // display sliders for the start and end points
+            network.uiStart = EditorGUILayout.IntSlider("Start waypoint", network.uiStart, 1, network.waypoints.Count);
+            network.uiEnd = EditorGUILayout.IntSlider("End waypoint", network.uiEnd, 1, network.waypoints.Count);
+        }
+
+        DrawDefaultInspector(); // Draws the default inspector as if we do not override the 'OnInspectorGUI'
+
+
+    }
+
     private void OnSceneGUI()
     {
         // The 'target' is the game object to which the 'AIWaypointNetwork' component is attached
@@ -49,22 +80,25 @@ public class AIWaypointsEditor : Editor
         }
         else if(network.displayMode == PathDisplayMode.Paths)
         {
-            // Calculate the navmesh path between two points:
-            NavMeshPath path = new NavMeshPath();
-            Vector3 from = network.waypoints[network.uiStart - 1].position;
-            Vector3 to   = network.waypoints[network.uiEnd - 1].position;
+            // We want to render a path only if the start and end waypoints are assigned in the array
+            if(network.waypoints[network.uiStart-1] != null && network.waypoints[network.uiEnd - 1] )
+            {
+                // Calculate the navmesh path between two points:
+                NavMeshPath path = new NavMeshPath();
+                Vector3 from = network.waypoints[network.uiStart - 1].position;
+                Vector3 to = network.waypoints[network.uiEnd - 1].position;
 
-            // Method 'CalculatePath' of the static 'NavMesh' class
-            // will return the calculated path in the 'path' parameter
-            NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
+                // Method 'CalculatePath' of the static 'NavMesh' class
+                // will return the calculated path in the 'path' parameter
+                NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
 
-            // The NavMeshPath contains member 'corners' which is a Vector3 array of all
-            // the waypoints calculated in the path. We shall use this array of corners as
-            // array of view points in the Handle.DrawPolyLine method
-            // => Draw the navmesh path netween two viewpoints
-            Handles.color = Color.yellow;
-            Handles.DrawPolyLine(path.corners);
+                // The NavMeshPath contains member 'corners' which is a Vector3 array of all
+                // the waypoints calculated in the path. We shall use this array of corners as
+                // array of view points in the Handle.DrawPolyLine method
+                // => Draw the navmesh path netween two viewpoints
+                Handles.color = Color.yellow;
+                Handles.DrawPolyLine(path.corners);
+            }
         }
-
     }
 }
--- Assets/_Dead Earth/Scripts/AIWaypointNetwork.cs
+++ Assets/_Dead Earth/Scripts/AIWaypointNetwork.cs
@@ -9,8 +9,13 @@ public enum PathDisplayMode
 
 public class AIWaypointNetwork : MonoBehaviour
 {
+    [HideInInspector]
     public PathDisplayMode displayMode = PathDisplayMode.Connections;
+    [HideInInspector]
     public int uiStart = 0;
+    [HideInInspector]
     public int uiEnd = 0;
+
     public List<Transform> waypoints = new List<Transform>();
+
 }
